2b17cb1826d14b1c6179d4c859218800380164be
PatricSPires <patric.pires@outlook.com>
2021-03-15 11:26:18 -0300

    chore: remove unused code and move types files

--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -5,5 +5,5 @@ type Theme = typeof theme
 
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends Theme { }
+  export interface DefaultTheme extends Theme {}
 }
